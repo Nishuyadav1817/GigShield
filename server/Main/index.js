549b65013f48b9267be95544b82b8ec2434fc253
nishuya <prathamyadav1817@gmail.com>
2026-03-17 00:03:43 +0530

    change in indexfile

--- server/Main/index.js
+++ server/Main/index.js
@@ -8,7 +8,23 @@ App.use(cookiparcer());
 require("dotenv").config();
 
 App.use("/worker",WorkerAuth);
+const allowedOrigins = [
+  "https://gig-shield.vercel.app/"
+  
+];
 
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 
 
